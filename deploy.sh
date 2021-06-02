ec00eed9ab49441211b1f002b0f89cd0b03b9e1d
Kang MyeongShin <kangugi205@gmail.com>
2021-06-02 22:13:06 +0900

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/zsh
 
+set -x
+
+git switch production
+git merge --no-ff --no-edit master 
+
 git checkout -b temp/production
 npm run build
 git add -f build
 git commit -m "Build 🍻"
 
 git reset production
-git switch production
 git add -f build
 git commit -m "Build 🍻"
